12c120065c24171b54e27b12315a5b143719a403
Tanvir Ahmad <tnvir9@gmail.com>
2023-09-17 20:29:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+// console.log(count)
+
+////*** Task ***/
+
+// 1. Create two variables, myAge and humanDogRatio
+let myAge = 27
+let humanDogRatio = 7
+// 2. Multiply the two together and store the result in myDogAge 
+let myDogAge = myAge * humanDogRatio
+// 3. Log myDogAge to the console
+console.log(myDogAge)
